9f96a0686d168117ec69d753f3956bac2f65fcc3
Tomas Nekolny <tomas.nekolny@productboard.com>
2020-08-11 08:29:17 +0200

    test it

--- tasks/list-active-rev.js
+++ tasks/list-active-rev.js
@@ -2,22 +2,23 @@ const gulp = require('gulp');
 const gutil = require('gulp-util');
 
 const { getRedisClient, getConfigFor } = require('./utils');
-const { some } = require('bluebird');
 
 async function getActiveRevision(client, config) {
   const activeRev = await client.get(config.mainRevKey);
 
-  gutil.log(`${config.mainRevKey}=${activeRev}`);
+  return activeRev;
 }
 
 async function printActiveRevision(config) {
   const client = await getRedisClient(config);
 
-  await Promise.all(
+  const activeRev = await Promise.all(
     (config.files || [config]).map((fileConfig) =>
       getActiveRevision(client, Object.assign({}, config, fileConfig)),
     ),
   );
+
+  gutil.log(`${config.mainRevKey}=${activeRev}`);
 
   client.quit();
 }
